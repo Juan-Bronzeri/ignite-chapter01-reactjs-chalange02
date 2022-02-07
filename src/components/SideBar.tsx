024bd9259ded62ce6d75ce1585591490f13e28b6
Juan Dias <juan.dias@texs.com.br>
2022-02-07 12:50:52 -0300

    finish

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App'
+import { Button } from './Button'
+
+interface ISideBar {
+	onButtonClick: (value: number) => void
+	genres: GenreResponseProps[]
+	selectedGenreId: number
+}
+
+export function SideBar({ onButtonClick, genres, selectedGenreId }: ISideBar) {
+	function handleClickButton(id: number) {
+		onButtonClick(id)
+	}
+
+	return (
+		<nav className='sidebar'>
+			<span>
+				Watch<p>Me</p>
+			</span>
+
+			<div className='buttons-container'>
+				{genres.map((genre) => (
+					<Button
+						key={String(genre.id)}
+						title={genre.title}
+						iconName={genre.name}
+						onClick={() => handleClickButton(genre.id)}
+						selected={selectedGenreId === genre.id}
+					/>
+				))}
+			</div>
+		</nav>
+	)
 }
